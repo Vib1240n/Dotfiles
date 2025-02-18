d491f640476034539daa5606ba3228e188d62248
vib1240n <vibhorsgr@gmail.com>
2025-02-18 13:35:44 -0800

    Files for Zen Browser

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,10 +2,8 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 config.font = wezterm.font("GeistMono Nerd Font")
 config.font = wezterm.font_with_fallback({
 	"Fira Code",
@@ -13,163 +11,114 @@ config.font = wezterm.font_with_fallback({
 })
 
 harfbuzz_features = { "zero", "ss01", "cv05" }
-config.window_background_opacity = 0.35
+config.window_background_opacity = 0.65
 config.macos_window_background_blur = 50
-
--- For example, changing the color scheme:
 config.window_frame = {
-	-- The font used in the tab bar.
-	-- Roboto Bold is the default; this font is bundled
-	-- with wezterm.
-	-- Whatever font is selected here, it will have the
-	-- main font setting appended to it to pick up any
-	-- fallback fonts you may have used there.
-	--   font = wezterm.font { family = 'Geist Mono', weight = 'Bold' },
-
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
 	font_size = 20.0,
-
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#005566",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
+	active_titlebar_bg = "none",
 	inactive_titlebar_bg = "#333333",
 }
 config.font_size = 18
 config.window_decorations = "RESIZE"
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
--- config.disable_default_key_bindings = true
 config.keys = {
 	{
-		key = "s",
-		mods = "LEADER|SHIFT",
+		key = "v",
+		mods = "CTRL|ALT|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
-	-- {
-	--     key = 'w',
-	--     mods = 'ALT',
-	--     action = act.SendString("∑")
-	-- },
-	-- {
-	-- 	key='a',
-	-- 	mods='ALT',
-	-- 	action = act.SendString("å")
-	-- },
-	-- {
-	-- 	key='s',
-	-- 	mods='ALT',
-	-- 	action = act.SendString("ß")
-	-- },
-	-- {
-	-- 	key='d',
-	-- 	mods='ALT',
-	-- 	action = act.SendString("∂")
-	-- }
 	{
 		key = "r",
 		mods = "CMD|SHIFT|ALT",
 		action = wezterm.action.ReloadConfiguration,
 	},
+	{
+		key = "c",
+		mods = "CTRL|ALT|SHIFT",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	-- {
+	-- 	key = "d",
+	-- 	mods = "CTRL|SHIFT|ALT|CMD",
+	-- 	actions = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	-- },
+	-- {
+	-- 	key = "w",
+	-- 	mods = "CTRL|SHIFT|ALT|CMD",
+	-- 	actions = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	-- },
+	-- {
+	-- 	key = "a",
+	-- 	mods = "CTRL|SHIFT|ALT|CMD",
+	-- 	actions = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	-- },
+	-- {
+	-- 	key = "s",
+	-- 	mods = "CTRL|SHIFT|ALT|CMD",
+	-- 	actions = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	-- },
 }
 config.tab_bar_at_bottom = true
-config.send_composed_key_when_left_alt_is_pressed = false
-config.send_composed_key_when_right_alt_is_pressed = true
-config.colors = {
-	tab_bar = {
-		-- The color of the strip that goes along the top of the window
-		-- (does not apply when fancy tab bar is in use)
-		background = "#3366ff",
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
+config.animation_fps = 1
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+-- config.use_fancy_tabbar = true
 
-		-- The active tab is the one that has focus in the window
-		active_tab = {
-			-- The color of the background area for the tab
-			bg_color = "#005566",
-			-- The color of the text for the tab
-			fg_color = "#ffffff",
+-- config.cursor_thickness = 2
+-- config.cursor_blink_rate = 800
 
-			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-			-- label shown for this tab.
-			-- The default is "Normal"
-			intensity = "Bold",
-
-			-- Specify whether you want "None", "Single" or "Double" underline for
-			-- label shown for this tab.
-			-- The default is "None"
-			underline = "Double",
-
-			-- Specify whether you want the text to be italic (true) or not (false)
-			-- for this tab.  The default is false.
-			italic = false,
-
-			-- Specify whether you want the text to be rendered with strikethrough (true)
-			-- or not for this tab.  The default is false.
-			strikethrough = false,
-		},
-
-		-- Inactive tabs are the tabs that do not have focus
-		inactive_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab`.
-		},
-
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over inactive tabs
-		inactive_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-			italic = true,
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab_hover`.
-		},
-
-		-- The new tab button that let you create new tabs
-		new_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab`.
-		},
-
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over the new tab button
-		new_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-			italic = true,
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab_hover`.
-		},
-	},
-	cursor_fg = "black",
-	cursor_bg = "#FE5E0A",
-	cursor_border = "#FE5E0A",
+config.visual_bell = {
+	fade_in_function = "EaseIn",
+	fade_in_duration_ms = 350,
+	fade_out_function = "EaseOut",
+	fade_out_duration_ms = 350,
 }
-
--- wezterm.on('update-right-status', function(window, pane)
---   local leader = ''
---   if window:leader_is_active() then
---     leader = 'LEADER'
---   end
---   window:set_right_status(leader)
--- end)
-
--- return {
---   leader = { key = 'a', mods = 'CTRL' },
---   colors = {
---     compose_cursor = 'orange',
---   },
---   config
+config.colors = {
+	visual_bell = "#202020",
+	cursor_bg = "#ff6700",
+}
+config.default_cursor_style = "BlinkingBar"
+-- config.hide_tab_bar_if_only_one_tab = true
+-- config.colors = {
+-- 	tab_bar = {
+-- 		-- 	-- background = "none",
+-- 		active_tab = {
+-- 			-- 		bg_color = "none",
+-- 			-- 		fg_color = "#ffffff",
+-- 			-- intensity = "Bold",
+-- 			-- underline = "Double",
+-- 			-- 		italic = true,
+-- 			-- 		strikethrough = false,
+-- 		},
+-- 		-- inactive_tab = {
+-- 		-- 		bg_color = "rgba(82, 82, 82, 0.5)",
+-- 		-- 		fg_color = "#808080",
+-- 		-- 	},
+-- 		-- 	inactive_tab_hover = {
+-- 		-- 		bg_color = "#3b3052",
+-- 		-- 		fg_color = "none",
+-- 		-- 		italic = true,
+-- 		-- 	},
+-- 		-- 	new_tab = {
+-- 		-- 		bg_color = "white",
+-- 		-- 		fg_color = "#808080",
+-- 		-- 	},
+-- 		-- 	new_tab_hover = {
+-- 		-- 		bg_color = "#3b3052",
+-- 		-- 		fg_color = "#909090",
+-- 		-- 		italic = true,
+-- 		-- 	},
+-- 	},
+-- 	cursor_fg = "black",
+-- 	cursor_bg = "#FE5E0A",
+-- 	cursor_border = "none",
 -- }
-
--- and finally, return the configuration to wezterm
 return config
--- .zshrc
+++ .zshrc
@@ -1,41 +1,18 @@
 # ZSH export and plugins for oh my zsh;
+# zmodload zsh/zprof
 export ZSH="$HOME/.oh-my-zsh"
-plugins=(git macos)
+eval $(/opt/homebrew/bin/brew shellenv)
+zstyle ':omz:plugins:nvm' lazy yes
+plugins=(git macos nvm)
 # neofetch
-#custom aliases
-alias mcos="source /Users/bb8/Development/Bash\ Scriptss/mcos.sh"
-alias dev="cd /Users/bb8/Development"
-alias c="clear"
-alias fa="cd /Users/bb8/Development/Flutter\ App/flutter_application_1"
-alias restart="exec $SHELL -l"
-alias sr="source ~/.zshrc"
-alias aws="cd ~/Desktop/.Misc && ssh -i "vibhore-alex-ace-key.pem" ubuntu@ec2-3-226-119-86.compute-1.amazonaws.com"
-alias ggs="cd ~/.aws && ssh -i "aws-ssh-connect.pem" ubuntu@ec2-35-90-4-112.us-west-2.compute.amazonaws.com"
-alias gcom="source ~/Development/Bash\ Scriptss/gcommands.sh"
-alias mgit="source ~/Development/Bash\ Scriptss/maingit.sh"
-alias haas="cd /Users/bb8/Development/Home-Assistant"
-alias acoj="cd /Users/bb8/Development/A-Cup-Of-Java"
-alias sql="mysql -u root -p"
-alias ppl="ppl.py"
-alias cat=bat
-alias ql=quick-look
-alias postgres="LC_ALL='C' /opt/homebrew/opt/postgresql@16/bin/postgres -D /opt/homebrew/var/postgresql@16"
-alias pgstart= "brew services start postgresql@16"
-alias pgstop="brew services stop postgresql@16"
-alias pgrestart="brew services restart postgresql@16"
-alias bsl="brew services list"
-alias phpstart="brew services start php"
-alias phpstop="brew services stop php"
-alias phprestart="brew services restart php"
-alias phpnod="opt/homebrew/opt/php/sbin/php-fpm --nodaemonize"
-alias zen_update="xattr -c '/Applications/Zen Browser.app/'"
 
-
+source $HOME/.config/functions/alias
 source /opt/homebrew/opt/chruby/share/chruby/auto.sh
 source $ZSH/oh-my-zsh.sh
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /Users/bb8/.config/functions/functions.sh
+c;fastfetch
 # source /Users/bb8/Development/git-sync/git-sync
 # source /Users/bb8/Development/git-sync/contrib/git-sync-on-inotify
  # for f in ~/Development/git-sync/*; do source $f; done
@@ -44,21 +21,23 @@ export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk@17/include"
-
+# JENV CONFIG
 export PATH="$HOME/.jenv/bin:$PATH"
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+# PYENV CONFIG
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(oh-my-posh init zsh --config '~/.config/oh-my-posh/customomp.omp.json')"
+# EXPORTS
 export ANDROID_SDK_ROOT="/opt/homebrew/share/android-commandlinetools"
 export PATH="/opt/homebrew/bin/pandoc:$PATH"
 export PATH="/opt/homebrew/opt/bison/bin:$PATH"
 export PATH="/opt/homebrew/opt/openssl@1.1/bin:$PATH"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export PATH="/opt/homebrew/opt/openjdk@17/bin:$PATH"
 export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
 PRELINE="\r\033[A"
@@ -77,6 +56,15 @@ alias haos="ssh -i ~/.ssh/id_rsa root@192.168.5.38 -p 22"
 alias finderNotifOn="sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd"
 alias finderNotifOff="sudo defaults delete Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification && sudo pkill diskarbitrationd"
 # Function to sleep MacBook with optional delay in seconds
-clear;fastfetch -c ~/.config/fastfetch/left.jsonc -l none; print -n "\e[H"; fastfetch -c ~/.config/fastfetch/right.jsonc -l none
+# clear;fastfetch -c ~/.config/fastfetch/left.jsonc -l none; print -n "\e[H"; fastfetch -c ~/.config/fastfetch/right.jsonc -l none
 #
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
 
+    autoload -Uz compinit
+    compinit
+  fi
+
+
+export CONFIG=/Users/bb8/.config
+# zprof
