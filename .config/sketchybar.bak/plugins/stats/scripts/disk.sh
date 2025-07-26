13aeb796fa63f56201db0b09f35d28cb5320a4bc
vib1240n <vibhorsgr@gmail.com>
2025-07-25 21:02:16 -0700

    removed unnecessary files

--- .config/sketchybar.bak/plugins/stats/scripts/disk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#disk.sh
-
-sketchybar -m --set "$NAME" label="$(df -H | grep -E '^(/dev/disk3s3).' | awk '{ printf ("%s\n", $5) }')"
--- Sketchybar.test2/items/battery.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-sketchybar -m --add alias "Control Center,com.bjango.istatmenus.battery" right           
-
-
